5ab74d37113e50d921f1c68baf9649d5cff657cb
SurajRampatwar <surajrampatwar123@gmail.com>
2022-05-18 16:24:43 +0530

    Flip coin and percentage of heads and tails

--- src/FlipCoin.java
+++ src/FlipCoin.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class FlipCoin {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
+        int countTails = 0;
+        int countHeads = 0;
         System.out.println("The number of times to flip coin ");
         int nTimesFlipped = scnr.nextInt();
-        int flipCoin = (int) Math.floor(Math.random() * 10) % 2;
-        if (flipCoin < 0.5) {
-            System.out.println("tails");
-        } else {
-            System.out.println("Heads");
+        for (int i = 0; i < nTimesFlipped; i++) {
+            int flipCoin = (int) Math.floor(Math.random() * 10) % 2;
+            if (flipCoin < 0.5) {
+                countTails += 1;
+            } else {
+                countHeads += 1;
+            }
         }
+        System.out.println("Tails = " + countTails);
+        System.out.println("Heads = " + countHeads);
     }
 }
