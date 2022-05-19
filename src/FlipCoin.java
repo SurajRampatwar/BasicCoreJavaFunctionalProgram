51cfb509888954e17303c72e50607ae0443e9a8c
SurajRampatwar <surajrampatwar123@gmail.com>
2022-05-19 15:05:51 +0530

    Refactor: head tail precent count

--- src/FlipCoin.java
+++ src/FlipCoin.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class FlipCoin {
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
+        Scanner num = new Scanner(System.in);
         int countTails = 0;
         int countHeads = 0;
         System.out.println("The number of times to flip coin ");
-        int nTimesFlipped = scnr.nextInt();
+        int nTimesFlipped = num.nextInt();
         for (int i = 0; i < nTimesFlipped; i++) {
-            int flipCoin = (int) Math.floor(Math.random() * 10) % 2;
+            double flipCoin = Math.random();
             if (flipCoin < 0.5) {
                 countTails += 1;
             } else {
@@ -17,4 +17,9 @@ public class FlipCoin {
         }
         System.out.println("Tails = " + countTails);
         System.out.println("Heads = " + countHeads);
+        int percentTail=(countTails*100)/nTimesFlipped;
+        int percentHead=(countHeads*100)/nTimesFlipped;
+        System.out.println("Tail Percent "+percentTail);
+        System.out.println("Head Precent "+percentHead);
     }
+}
--- src/LeapYear.java
+++ src/LeapYear.java
@@ -4,9 +4,9 @@ public class LeapYear {
     public static void main(String[] args) {
         int Year;
         System.out.println("Enter any year : ");
-        Scanner newYear = new Scanner(System.in);
+        Scanner newYear=new Scanner(System.in);
         Year = newYear.nextInt();
-        if ((Year % 4 == 0) && (Year % 100 != 0) || (Year % 400 == 0)) {
+        if (Year % 4 == 0) {
             System.out.println(" IS LEAP YEAR");
         } else {
             System.out.println("IS NOT LEAP YEAR");
