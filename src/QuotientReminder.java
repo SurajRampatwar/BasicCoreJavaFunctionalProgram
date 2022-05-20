efbf6f3d2da9b210008ec6323e2661c5a49a62ad
SurajRampatwar <surajrampatwar123@gmail.com>
2022-05-20 19:29:59 +0530

    Quotient Reminde of Number

---
+++ src/QuotientReminder.java
@@ -0,0 +1,11 @@
+public class QuotientReminder {
+    public static void main(String[] args) {
+        int number = 36;
+        int devisor = 5;
+        int Quotient = number / devisor;
+        int Reminder = number % devisor;
+        System.out.println("Quotient = " + Quotient);
+        System.out.println("Reminder = " + Reminder);
+
+    }
+}
