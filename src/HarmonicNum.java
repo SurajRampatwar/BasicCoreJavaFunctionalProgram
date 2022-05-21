9875dfaafa713e5b3fa446962f17903e8cccde41
SurajRampatwar <surajrampatwar123@gmail.com>
2022-05-21 17:42:54 +0530

    Even Odd numbers

--- src/HarmonicNum.java
+++ src/HarmonicNum.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
 
 public class HarmonicNum {
-
-
     public static void main(String[] args) {
         double harmonic = 0;
         int N;
-        System.out.println("Enter Harmonic Number");
+        System.out.println("Enter any number");
         Scanner hrmnk = new Scanner(System.in);
         N = hrmnk.nextInt();
         if (N != 0) {
@@ -15,7 +13,7 @@ public class HarmonicNum {
             }
             System.out.println(harmonic);
         } else {
-            System.out.println("Invalid number");
+            System.out.println("Invalid Number");
         }
     }
 }
